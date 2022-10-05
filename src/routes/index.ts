d18ffbec8ec15d297e8e2c94644da1c27920f719
Vitaliy Senenko <senenkovitalik@gmail.com>
2022-10-05 19:21:12 +0300

    refactor: Remove useless

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,9 @@
-import express, { Router, Request, Response, RequestHandler } from "express";
-import { PhoneSystemType } from "../db/models/PhoneSystemType";
+import express, { Router, Request, Response } from "express";
 const router: Router = express.Router();
 
 /* GET home page. */
-router.get("/", (async (_req: Request, res: Response) => {
-  try {
-    const types = await PhoneSystemType.findAll();
-    console.log(types);
-    res.render("index", { title: "Express" });
-  } catch (err) {
-    console.error(err);
-  }
-}) as RequestHandler);
+router.get("/", (_req: Request, res: Response) => {
+  res.render("index", { title: "Express" });
+});
 
 export default router;
