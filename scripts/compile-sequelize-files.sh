432adae20d9e71f6d6dd23e4a93025eff3864c59
Vitaliy Senenko <senenkovitalik@gmail.com>
2022-10-11 18:05:59 +0300

    fix: Fix scripts

---
+++ scripts/compile-sequelize-files.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+src_dir=$(pwd)/src/db
+dst_dir=$(pwd)/build/db
+
+rm -rf $dst_dir/*
+
+echo "Transpile sequelize config, migrations and seeders"
+
+./node_modules/typescript/bin/tsc \
+  $src_dir/config/* \
+  $src_dir/migrations/* \
+  $src_dir/seeders/* \
+  --module commonjs \
+  --moduleResolution node \
+  --target es2021 \
+  --outDir $dst_dir
+\
