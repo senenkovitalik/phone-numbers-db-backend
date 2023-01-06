e3885f01b47f3a3a1ab37e50f6dc1e899f2b2620
Vitaliy Senenko <senenkovitalik@gmail.com>
2023-01-06 22:04:39 +0200

    fix: Fix communication phone numbers query handler

--- src/graphql/resolvers/Queries/communicationPhoneNumbers.ts
+++ src/graphql/resolvers/Queries/communicationPhoneNumbers.ts
@@ -1,60 +1,17 @@
-import { WhereOptions, InferAttributes } from "sequelize";
 import { CommunicationPhoneNumber } from "../../../db/models";
 import {
   Aggregate,
   QueryCommunication_Phone_NumbersArgs,
   QueryCommunication_Phone_Numbers_AggregateArgs,
 } from "../../__generated/graphql";
+import { calculateOptions } from "../utils";
 
 export const communication_phone_numbers = async (
   _parent: unknown,
-  { limit, offset, where }: QueryCommunication_Phone_NumbersArgs
+  args: QueryCommunication_Phone_NumbersArgs
 ): Promise<CommunicationPhoneNumber[]> => {
   try {
-    const options: {
-      limit?: number;
-      offset?: number;
-      where?: WhereOptions<
-        InferAttributes<
-          CommunicationPhoneNumber,
-          {
-            omit: never;
-          }
-        >
-      >;
-    } = {
-      limit: 10,
-      offset: 0,
-      where: {},
-    };
-
-    if (limit) {
-      options.limit = limit;
-    }
-
-    if (offset) {
-      options.offset = offset;
-    }
-
-    if (where) {
-      const { id, value } = where;
-
-      if (id) {
-        if (id._in || id._eq) {
-          options.where = {
-            id: (id._in || id._eq) as number | number[],
-          };
-        }
-      }
-
-      if (value) {
-        if (value._eq) {
-          options.where = {
-            value: value._eq,
-          };
-        }
-      }
-    }
+    const options = calculateOptions(args);
 
     return await CommunicationPhoneNumber.findAll(options);
   } catch (e) {
@@ -65,28 +22,16 @@ export const communication_phone_numbers = async (
 
 export const communication_phone_numbers_aggregate = async (
   _parent: unknown,
-  { where }: QueryCommunication_Phone_Numbers_AggregateArgs
+  args: QueryCommunication_Phone_Numbers_AggregateArgs
 ): Promise<Aggregate> => {
   try {
-    const options = {
-      where: {},
-    };
+    const options = calculateOptions(args);
 
-    if (where) {
-      const { id } = where;
-
-      if (id?._in) {
-        options.where = {
-          id: id._in,
-        };
-      }
-    }
-
-    const res = await CommunicationPhoneNumber.findAll(options);
+    const { count } = await CommunicationPhoneNumber.findAndCountAll(options);
 
     return {
       aggregate: {
-        count: res.length,
+        count,
       },
     };
   } catch (e) {
