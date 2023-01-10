a510e3c5004009245208443e86bfd29e2fe4758d
Vitaliy Senenko <senenkovitalik@gmail.com>
2023-01-10 15:51:17 +0200

    feat: Add more fake locations

--- src/db/seeders/202212292050-add-fake-location.ts
+++ src/db/seeders/202212292050-add-fake-location.ts
@@ -1,24 +1,48 @@
-import type { InferAttributes } from "sequelize";
+import { InferCreationAttributes, Op } from "sequelize";
 import { Location } from "../models";
 
-const locationData: InferAttributes<Location, { omit: "id" }> = {
-  country: "Ukraine",
-  region: "Zhytomir",
-  city: "Yaropovichi",
-  createdAt: new Date(),
-  updatedAt: new Date(),
-};
+const locationData: InferCreationAttributes<Location, { omit: "id" }>[] = [
+  {
+    country: "Ukraine",
+    region: "Zhytomir",
+    city: "Yaropovichi",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    country: "Ukraine",
+    region: "Rivne",
+    district: "Rivne",
+    street: "Petra Mohily",
+    building: "5",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
 
 module.exports = {
   up: () => {
-    return Location.create(locationData);
+    return Location.bulkCreate(locationData);
   },
   down: () => {
     return Location.destroy({
       where: {
-        country: locationData.country,
-        region: locationData.region as string,
-        city: locationData.city as string,
+        [Op.or]: [
+          {
+            [Op.and]: {
+              region: "Zhytomir",
+              city: "Yaropovichi",
+            },
+          },
+          {
+            [Op.and]: {
+              region: "Rivne",
+              district: "Rivne",
+              street: "Petra Mohily",
+              building: "5",
+            },
+          },
+        ],
       },
     });
   },
